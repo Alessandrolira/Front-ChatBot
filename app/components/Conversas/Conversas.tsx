4c6468b5f53f9456dce64ae7b0f820d2c9785046
Alessandro Lira <alessandrolira09@gmail.com>
2025-05-02 16:36:07 -0300

    Botao para desligar a IA criado

--- app/components/Conversas/Conversas.tsx
+++ app/components/Conversas/Conversas.tsx
@@ -1,23 +1,46 @@
+"use client"
+
+import { useState } from "react"
+
 interface ConversasProps {
     nomeUser: string
     ultima_mensagemUser?: string
-    numeroUser?: number
+    numeroUser?: number | any
     dataUser?: number
+    numeroSelecionado?: any
+    funcaoScroll?: any
+    checkado?: any
+    funcaoCheck?: any
 }
 
-export default function Conversas( {nomeUser, ultima_mensagemUser, numeroUser, dataUser} : ConversasProps) {
+export default function Conversas( {nomeUser, ultima_mensagemUser, numeroUser, dataUser, numeroSelecionado, funcaoScroll, checkado, funcaoCheck} : ConversasProps) {
+
     return (
         <>
             <div className="bg-(--background) pt-[1em] pb-[1em] pl-[2em] pr-[2em] border-2 border-(--cinza2) flex items-center">
-                <div className="rounded-full w-[3em] h-[3em] bg-(--branco) mr-[2em]"></div>
-                <div>
-                    <h2>{nomeUser}</h2>
-                    <p className="text-[0.7em] mt-[0.5em] line-clamp-2 w-45">{ultima_mensagemUser}</p>
-                    <p className="text-[0.8em] mt-[0.5em]">{numeroUser}</p>
-                    <div className="flex">
-                        <p className="text-[0.6em] mt-[0.5em]">{dataUser}</p>
+                <div className="flex flex-row justify-between w-full">
+                    <div className="flex flex-row items-center">
+                        <div className="rounded-full w-[3em] h-[3em] bg-(--branco) mr-[2em]"></div>
+                        <div>
+                            <h2>{nomeUser}</h2>
+                            <p className="text-[0.7em] mt-[0.5em] line-clamp-2 w-45">{ultima_mensagemUser}</p>
+                            <p className="text-[0.8em] mt-[0.5em]">{numeroUser}</p>
+                            <div className="flex">
+                                <p className="text-[0.6em] mt-[0.5em]">{dataUser}</p>
+                            </div>
+                        </div>
                     </div>
-                    
+                    {numeroSelecionado != null ? (
+                        <div className="flex items-center">
+                            <label className="switch">
+                                <input type="checkbox" id="switch" onChange={funcaoCheck} checked={checkado}/>
+                                <span className="slider"></span>
+                            </label>
+                            <button onClick={funcaoScroll} className="top-43.5 right-10 text-white flex items-center">
+                                <img src="/arrow-down.png" alt="" className="bg-(--cinza) rounded-full shadow-lg hover:bg-(--branco) transition cursor-pointer p-3"/>
+                            </button>
+                        </div>
+                    ) :""}
                 </div>
             </div>
         </>
--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,58 @@
 import { useState, useEffect, useRef } from "react";
 import Conversas from "./components/Conversas/Conversas";
 import Mensagens from "./components/Mensagens/Mensagens";
+import { io, Socket } from "socket.io-client";
 
 export default function Home() {
-  const baseUrl = "http://192.168.15.105:5001/gerarjson";
-  const [dadosConversas, setDadosConversas] = useState<any>({});
-  const [selectedNumber, setSelectedNumber] = useState<string | null>(null);
-  const messagesEndRef = useRef(null);
+
+  const [isChecked, setIsChecked] = useState(true)
+
+  const handleRequest = (e:any) => {
+    const checked = e.target.checked
+    setIsChecked(checked)
+    console.log(checked)
+}
+
+
+  const [socket, setSocket] = useState<Socket | null>(null);
+  const [mensagem, setMensagem] = useState("");
+  const [resposta, setResposta] = useState("");
+
+  const meuIp = "192.168.20.46:5001"
+
+  const baseUrl = `http://${meuIp}/gerarjson`;
+
+  const [mensagemEnviada, setMensagemEnviada] = useState(false);
 
   useEffect(() => {
+    const socketIo = io("http://localhost:5001");
+    setSocket(socketIo);
+  
+    socketIo.on("connect", () => {
+      console.log("Conectado ao servidor Socket.IO");
+    });
+  
+    socketIo.on("connect_error", (err) => {
+      console.error("Erro de conexão:", err);
+    });
+  
+    socketIo.on("mensagem", (data) => {
+      console.log("Recebido do backend via WebSocket:", data);
+      setResposta(data.resposta); // exemplo de ação
+      setMensagemEnviada(true); // força refresh no useEffect de fetch
+    });
+  
+    return () => {
+      socketIo.disconnect();
+    };
+  }, []);
+  
+  const [dadosConversas, setDadosConversas] = useState<any>({});
+  const [selectedNumber, setSelectedNumber] = useState<string>("");
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+  
     async function fetchData() {
       try {
         const response = await fetch(baseUrl);
@@ -21,13 +65,20 @@ export default function Home() {
         console.error("Erro ao buscar os dados:", error);
       }
     }
+
     fetchData();
-  }, []);
+
+    // Após a execução do fetch, você reseta a mensagemEnviada para false
+    setMensagemEnviada(false);
+
+  }, [mensagemEnviada]);  // Esse useEffect só é disparado quando mensagemEnviada for true
 
   const mensagensSelecionadas = selectedNumber ? dadosConversas[selectedNumber]?.mensagens || [] : [];
 
-  const scrollToBottom = () => {
-    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  const scrollToBottom = () => {''
+      if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+      }
   };
 
   return (
@@ -38,8 +89,8 @@ export default function Home() {
           <p className="text-[1.1em] pl-[1em] pt-[1.3em] pb-[1.3em]">Conversas em produção</p>
 
           {Object.entries(dadosConversas)
-            .sort((a, b) => {
-              const parseDate = (dataString) => {
+            .sort((a: any, b: any) => {
+              const parseDate = (dataString:any) => {
                 if (!dataString) return 0;
                 const [dia, mes, anoHora] = dataString.split('/');
                 const [ano, hora] = anoHora.split(' ');
@@ -50,10 +101,10 @@ export default function Home() {
               const ultimaMensagemB = parseDate(b[1].mensagens[b[1].mensagens.length - 1]?.data_hora);
               return ultimaMensagemB - ultimaMensagemA;
             })
-            .map(([numero, dados]) => (
+            .map(([numero, dados]:any) => (
               <div key={numero} onClick={() => setSelectedNumber(numero)} className="cursor-pointer">
                 <Conversas
-                  nomeUser={dados.nome || "Usuário Desconhecido"}
+                  nomeUser={dados.nome|| "Usuário Desconhecido"}
                   ultima_mensagemUser={dados.mensagens[dados.mensagens.length - 1].mensagem}
                   dataUser={dados.mensagens[dados.mensagens.length - 1].data_hora}
                 />
@@ -68,25 +119,23 @@ export default function Home() {
 
           {selectedNumber && (
             <div>
-              <Conversas nomeUser={dadosConversas[selectedNumber]?.nome || "Usuário Desconhecido"} numeroUser={selectedNumber}></Conversas>
+              <Conversas nomeUser={dadosConversas[selectedNumber]?.nome || "Usuário Desconhecido"} numeroUser={selectedNumber} numeroSelecionado={selectedNumber} funcaoScroll={scrollToBottom} checkado={isChecked} funcaoCheck={handleRequest}></Conversas>
             </div>
           )}
-
-          <section className="h-[calc(100vh-2em-2em-70px-2em-3em-2em-1em)] overflow-y-auto relative">
+          <section className="h-[calc(100vh-2em-2em-3em-2em-1em)] overflow-y-auto relative">
             <div className="overflow-y-auto pt-[2em] p-[3em] scrollbar-thin scrollbar-thumb-[--cinza] scrollbar-track-[--preto]">
               {mensagensSelecionadas.length > 0 ? (
-                mensagensSelecionadas.reduce((acc, mensagem, index, array) => {
+                mensagensSelecionadas.reduce((acc:any, mensagem:any, index:any, array:any) => {
                   const dataMensagem = mensagem.data_hora.split(' ')[0];
                   const dataAnterior = index > 0 ? array[index - 1].data_hora.split(' ')[0] : null;
                   
                   if (dataMensagem !== dataAnterior) {
                     acc.push(
-                      <div key={`date-${dataMensagem}`} className="text-center text-gray border-y my-[2em] my-2">
+                      <div key={`date-${dataMensagem}`} className="text-center text-gray border-y my-[2em]">  
                         {dataMensagem}
                       </div>
                     );
                   }
-
                   acc.push(
                     <Mensagens key={`msg-${index}`} nome={dadosConversas[selectedNumber]?.nome} usuario={true} data={mensagem.data_hora}>
                       {mensagem.mensagem}
@@ -99,15 +148,20 @@ export default function Home() {
                   );
                   return acc;
                 }, [])
+                
               ) : (
                 <p className="text-center text-gray-400">Selecione uma conversa para visualizar as mensagens</p>
               )}
               <div ref={messagesEndRef}></div>
             </div>
-
-            <button onClick={scrollToBottom} className="fixed top-43.5 right-10 bg-(--cinza) text-white p-3 rounded-full shadow-lg hover:bg-(--branco) transition cursor-pointer">
-              <img src="/arrow-down.png" alt="" />
-            </button>
+            {isChecked == false ? (
+                <div className="flex sticky bottom-1 pb-[1em] pt-[1em] z-10 w-full pr-[1em] bg-(--cinza)">
+                  <input placeholder="Escreva a mensagem aqui..." className="w-full rounded-2xl ml-2 p-3 bg-(--background)"/>
+                  <button className="text-[0.8em] bg-[#5fdd54] text-black rounded-2xl ml-2">Enviar Mensagem</button>
+                </div >
+              ) : (
+              " "
+              )}
           </section>
         </div>
       </div>
